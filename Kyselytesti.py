e845f6d58f95eaa89f31e2d566dcbbab64f5b410
Ilkka <lindemani@gmail.com>
2022-03-26 21:12:19 +0200

    Add files via upload

---
+++ Kyselytesti.py
@@ -0,0 +1,12 @@
+
+print("Hello, Welcome to show")
+name = input("What is your name? \n")
+
+print("Hello " + name + ", thank you coming today. ")
+
+menu = "Black coffee, Espresso, Latte, Cappucino"
+print(name + ", what is menu today?\nHere is what we are serving. \n" + menu)
+
+order = input()
+
+print("Sounds good " + name + ", we'll have that " + order + " ready for you in a moment")
